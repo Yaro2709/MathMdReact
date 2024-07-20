45598df9ca2d608c72850644b9a204e540151b64
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-20 20:30:10 +0300

    i18n

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,9 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "path";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] { //не забываем про типизацию WebpackPluginInstance
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] { //не забываем про типизацию WebpackPluginInstance
 
     return [
         new HtmlWebpackPlugin({
@@ -12,8 +11,11 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
         }), //плагин для html сборки
         new webpack.ProgressPlugin(), //плагин для того, чтобы мы видели сколько процентов проекта мы уже собрали.
         new MiniCssExtractPlugin({ //плагин для css
-            filename: 'css/[name].[contenthash:8].css', //делаем хеширование в названиях для обычных файлов
-            chunkFilename: 'css/[name].[contenthash:8].css', //делаем хеширование в названиях для чанк файлов
+            filename: 'css/[name].[contenthash:8].css', //делаем хеширование в названиях
+            chunkFilename: 'css/[name].[contenthash:8].css', //делаем хеширование в названиях
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
         })
     ]
 }
