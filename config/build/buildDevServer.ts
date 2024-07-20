2e9888ddf747cded16a98e2ef468270f81583a39
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-20 20:32:09 +0300

    Webpack hot module

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,11 +1,11 @@
 import {BuildOptions} from "./types/config";
 //Тип конфирации WebPack и DevServer пересекается (одинаковые типы), поэтому мы переопределяем его, как DevServerConfiguration
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-
 export function buildDevServer(options: BuildOptions): DevServerConfiguration { //не забываем про тип
     return {
         port: options.port, //порт, инициализируем из опций
         open: true, //для автоматического открытия страинцы в бразуере
         historyApiFallback: true, //позволяет исправить ошибку, если мы находимся не в корневой странице
+        hot: true,
     }
 }
