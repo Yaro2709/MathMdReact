2ec8ebac4282b9a8bb60cce1c11a97b9fca6e0a3
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-22 21:06:34 +0300

    BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,23 +1,26 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "path";
-import webpack from "webpack";
-import {BuildOptions} from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { BuildOptions } from './types/config';
 
-export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] { //не забываем про типизацию WebpackPluginInstance
-
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] { // не забываем про типизацию WebpackPluginInstance
     return [
         new HtmlWebpackPlugin({
-            template: paths.html, //объявляем шаблонный файл
-        }), //плагин для html сборки
-        new webpack.ProgressPlugin(), //плагин для того, чтобы мы видели сколько процентов проекта мы уже собрали.
-        new MiniCssExtractPlugin({ //плагин для css
-            filename: 'css/[name].[contenthash:8].css', //делаем хеширование в названиях
-            chunkFilename: 'css/[name].[contenthash:8].css', //делаем хеширование в названиях
+            template: paths.html, // объявляем шаблонный файл
+        }), // плагин для html сборки
+        new webpack.ProgressPlugin(), // плагин для того, чтобы мы видели сколько процентов проекта мы уже собрали.
+        new MiniCssExtractPlugin({ // плагин для css
+            filename: 'css/[name].[contenthash:8].css', // делаем хеширование в названиях
+            chunkFilename: 'css/[name].[contenthash:8].css', // делаем хеширование в названиях
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(), //hot плагин, чтобы при изменениях страница не обновлялась
-    ]
+        new webpack.HotModuleReplacementPlugin(), // hot плагин, чтобы при изменениях страница не обновлялась
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }), // анализатор наших чанков и бандлов
+    ];
 }
